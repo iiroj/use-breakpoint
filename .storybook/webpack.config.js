5cf97395d7c531adc32168d5bfe3bd289befc738
Iiro Jäppinen <iiro@jappinen.fi>
2019-02-27 19:38:03 +0200

    fix(storybook): resolve TypeScript files before JavaScript, install missing devDependency

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = (baseConfig, env, config) => {
     }
   );
 
-  config.resolve.extensions.push(".ts", ".tsx");
+  config.resolve.extensions.unshift(".ts", ".tsx");
 
   return config;
 };
