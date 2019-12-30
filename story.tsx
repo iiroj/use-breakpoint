107c826e685d359b48c795381264ba799a14c7d0
Iiro Jäppinen <iiro@jappinen.fi>
2019-12-30 13:15:32 +0200

    chore: prevent render loop in story
    This was caused by the inlined configuration object, which got recreated on every render

--- story.tsx
+++ story.tsx
@@ -3,32 +3,32 @@ import { storiesOf } from "@storybook/react";
 
 import useBreakpoint from "./index";
 
-storiesOf("use-breakpoint", module).add("useBreakpoint", () => {
-  const CurrentBreakpoint = () => {
-    const { breakpoint, minWidth, maxWidth } = useBreakpoint(
-      { mobile: 0, tablet: 768, desktop: 1280 },
-      "mobile"
-    );
-    return (
-      <p>
-        The current breakpoint is <strong>{breakpoint}</strong> with{" "}
-        <em>min-width</em> of{" "}
-        <strong>
-          {minWidth}
-          px
-        </strong>
-        {maxWidth ? (
-          <>
-            {" "}
-            and a <em>max-width</em> of <strong>{maxWidth}px</strong>
-          </>
-        ) : (
-          ""
-        )}
-        !
-      </p>
-    );
-  };
+const config = { mobile: 0, tablet: 768, desktop: 1280 };
 
-  return <CurrentBreakpoint />;
-});
+const CurrentBreakpoint = () => {
+  const { breakpoint, minWidth, maxWidth } = useBreakpoint(config, "mobile");
+
+  return (
+    <p>
+      The current breakpoint is <strong>{breakpoint}</strong> with{" "}
+      <em>min-width</em> of{" "}
+      <strong>
+        {minWidth}
+        px
+      </strong>
+      {maxWidth ? (
+        <>
+          {" "}
+          and a <em>max-width</em> of <strong>{maxWidth}px</strong>
+        </>
+      ) : (
+        ""
+      )}
+      !
+    </p>
+  );
+};
+
+storiesOf("use-breakpoint", module).add("useBreakpoint", () => (
+  <CurrentBreakpoint />
+));
