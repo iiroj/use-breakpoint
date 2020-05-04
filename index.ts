b0ce1b0ce79ce57a54064b8050e93b53d67c1a3d
Iiro Jäppinen <iiro@jappinen.fi>
2020-05-04 08:55:26 +0300

    fix: unsubscribe from window.matchMedia when unmounting

--- index.ts
+++ index.ts
@@ -1,23 +1,27 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState, useCallback } from "react";
 
 export type Config = {
   readonly [key: string]: number;
 };
 
-export type Breakpoint = {
-  breakpoint: string;
+export type Breakpoint<C extends Config> = {
+  breakpoint: keyof C;
   maxWidth?: number;
-  minWidth: number;
+  minWidth: C[keyof C];
 };
 
-type MediaQuery = {
-  breakpoint: string;
+type MediaQuery<C extends Config> = {
+  breakpoint: keyof C;
   maxWidth?: number;
-  minWidth: number;
+  minWidth: C[keyof C];
   query: string;
 };
 
-const createMediaQueries = (breakpoints: Config) => {
+/**
+ * Create media query objects
+ * @param breakpoints the list of configured breakpoint names and their pixel values
+ */
+export const createMediaQueries = (breakpoints: Config) => {
   const sortedBreakpoints = Object.keys(breakpoints).sort(
     (a, b) => breakpoints[b] - breakpoints[a]
   );
@@ -39,7 +43,7 @@ const createMediaQueries = (breakpoints: Config) => {
       query += `(max-width: ${maxWidth - 1}px)`;
     }
 
-    const mediaQuery: MediaQuery = {
+    const mediaQuery: MediaQuery<Config> = {
       breakpoint,
       maxWidth,
       minWidth,
@@ -50,47 +54,73 @@ const createMediaQueries = (breakpoints: Config) => {
   });
 };
 
-export default function useBreakpoint(config: Config): Breakpoint | undefined;
+export default function useBreakpoint<C extends Config>(
+  config: C
+): Breakpoint<C> | undefined;
 
 export default function useBreakpoint<C extends Config>(
   config: C,
   defaultBreakpoint: keyof C
-): Breakpoint;
+): Breakpoint<C>;
 
+/**
+ * A React hook to use the current responsive breakpoint.
+ * Will listen to changes using the window.matchMedia API.
+ * @param config the list of configured breakpoint names and their pixel values
+ * @param defaultBreakpoint the optional default breakpoint
+ */
 export default function useBreakpoint<C extends Config>(
   config: C,
   defaultBreakpoint?: keyof C
 ) {
+  /**
+   * Memoize list of calculated media queries from config
+   */
   const mediaQueries = useMemo(() => createMediaQueries(config), [config]);
 
-  const [currentBreakpoint, setCurrentBreakpoint] = useState(() => {
+  /**
+   * Get initial breakpoint value
+   */
+  const [currentBreakpoint, setCurrentBreakpoint] = useState<
+    Breakpoint<C> | undefined
+  >(() => {
     if (!defaultBreakpoint) return undefined;
 
     const { query, ...breakpoint } = mediaQueries.find(
       (query) => query.breakpoint === defaultBreakpoint
     )!;
 
-    return breakpoint;
+    return breakpoint as Breakpoint<C>;
   });
 
-  const updateBreakpoint = (
-    { matches }: MediaQueryList,
-    breakpoint: Breakpoint
-  ) => {
-    if (matches) {
-      setCurrentBreakpoint(breakpoint);
-    }
-  };
+  /**
+   * If there's a match, update the current brekpoint
+   */
+  const updateBreakpoint = useCallback(
+    ({ matches }: MediaQueryList, breakpoint: Breakpoint<C>) => {
+      if (matches) {
+        setCurrentBreakpoint(breakpoint);
+      }
+    },
+    [setCurrentBreakpoint]
+  );
 
+  /**
+   * On changes to mediaQueries, subscribe to changes using window.matchMedia.
+   */
   useEffect(() => {
-    mediaQueries.forEach(({ query, ...breakpoint }) => {
+    const unsubscribers = mediaQueries.map(({ query, ...breakpoint }) => {
       const mediaQuery = window.matchMedia(query);
-      updateBreakpoint(mediaQuery, breakpoint);
-      const handleChange = () => updateBreakpoint(mediaQuery, breakpoint);
+      updateBreakpoint(mediaQuery, breakpoint as Breakpoint<C>);
+      const handleChange = () =>
+        updateBreakpoint(mediaQuery, breakpoint as Breakpoint<C>);
       mediaQuery.addListener(handleChange);
+      // map the unsubscribers array to a list of unsubscriber methods
       return () => mediaQuery.removeListener(handleChange);
     });
-  }, [mediaQueries]);
+    // return a function that when called, will call all unsubscribers
+    return () => unsubscribers.forEach((unsubscriber) => unsubscriber());
+  }, [mediaQueries, updateBreakpoint]);
 
   return currentBreakpoint;
 }
