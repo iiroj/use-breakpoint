c63d3df34d9aaca2990869ad7c521c4a7186260d
Iiro Jäppinen <iiro@jappinen.fi>
2022-12-04 13:38:24 +0200

    ci: setup semantic release

---
+++ .releaserc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
+    [
+      '@semantic-release/release-notes-generator',
+      { preset: 'conventionalcommits' },
+    ],
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/npm',
+      { npmPublish: false /** GitHub Actions handle npm publish */ },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package-lock.json', 'package.json'],
+        message:
+          'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        failTitle: false,
+        labels: false,
+        releasedLabels: false,
+      },
+    ],
+  ],
+  repositoryUrl: process.env.GITHUB_ACTIONS
+    ? 'https://github.com/iiroj/use-breakpoint'
+    : 'git@github.com:iiroj/use-breakpoint.git',
+}
